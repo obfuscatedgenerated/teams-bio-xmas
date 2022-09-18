f0aac69de3f67a4780f43e31bea69c1138021380
Ollie <75035364+obfuscatedgenerated@users.noreply.github.com>
2022-09-18 23:05:01 +0100

    create action

---
+++ action.js
@@ -0,0 +1,28 @@
+const axios = require("axios");
+if (process.env.NODE_ENV !== "production") {
+    require("dotenv").config();
+}
+
+const { TEAMS_TOKEN } = process.env;
+
+const today = new Date();
+
+let effective_year = (today.getMonth() === 12 && today.getDate() >= 25) ? today.getFullYear() + 1 : today.getFullYear();
+const xmas = new Date(effective_year, 11, 25);
+
+const days = Math.ceil((xmas - today) / (1000 * 60 * 60 * 24));
+
+let effective_bio = (days === 0) ? "Happy Holidays!" : `Only ${days} days until December 25th!`;
+
+axios.patch(
+    "https://graph.microsoft.com/v1.0/me",
+    {
+        aboutMe: effective_bio
+    },
+    {
+        headers: {
+            Authorization: `Bearer ${TEAMS_TOKEN}`,
+            "Content-Type": "application/json"
+        }
+    }
+);
